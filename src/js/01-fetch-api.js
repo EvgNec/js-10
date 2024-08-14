f2dad78ad4dd7f8cbe93b81df2d831aa20db735f
Evgeniy <evgnec@gmail.com>
2024-08-14 23:34:42 +0300

    upd

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -12,36 +12,52 @@ fetch(
     .then(r => r.json())
     .then(console.log);
 
- /*-------------------------*/   
-const PASTE_YOUR_API_KEY_HERE = 'd80d26f396a09dee964f29c0205bdd02' 
-   
-const url = 'https://api.weatherstack.com/current?access_key={PASTE_YOUR_API_KEY_HERE}&query=New Delhi';
+ /*-------------------------*/
+ 
+ // =========================================
+
+const url = 'https://newsapi.org/v2/everything?q=dog';
 const options = {
-	method: 'GET'
+  headers: {
+    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+  },
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+fetch(url, options)
+  .then(r => r.json())
+  .then(console.log);
+
+ // =========================================
+
+// const PASTE_YOUR_API_KEY_HERE = 'd80d26f396a09dee964f29c0205bdd02' 
+   
+// const url = `https://api.weatherstack.com/current?access_key=${PASTE_YOUR_API_KEY_HERE}&query=New Delhi`;
+// const options = {
+// 	method: 'GET'
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
 
 
-import axios from 'axios';
+// import axios from 'axios';
 
-const options2 = {
-  method: 'GET',
-  url: 'https://api.weatherstack.com/current?access_key={PASTE_YOUR_API_KEY_HERE}',
-  params: {
-    query: "New Delhi",
-  }
-};
+// const options2 = {
+//   method: 'GET',
+//   url: 'https://api.weatherstack.com/current?access_key={PASTE_YOUR_API_KEY_HERE}',
+//   params: {
+//     query: "New Delhi",
+//   }
+// };
 
-try {
-	const response = await axios.request(options2);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+// try {
+// 	const response = await axios.request(options2);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }

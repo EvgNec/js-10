1f9b728c433ce6a04015b0099206a41357d24192
EvgNec <evgnec@gmail.com>
2024-08-14 20:25:46 +0000

    Deploying to gh-pages from @ EvgNec/js-10@67b9218a4a63d62fa61215931077e5ca0fdd41d1 🚀

---
+++ 01-fetch-api.ab1fff66.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=36946759-7e5fc12dbc6128b490595b505&q=dog&image_type=photo").then((function(e){return e.json()})).then(console.log),fetch("http://api.weatherstack.com/current?access_key=d80d26f396a09dee964f29c0205bdd02&query= New York").then((function(e){return e.json()})).then(console.log);
+//# sourceMappingURL=01-fetch-api.ab1fff66.js.map
--- 01-fetch-api.b69b7097.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/?key=36946759-7e5fc12dbc6128b490595b505&q=dog&image_type=photo").then((function(e){return e.json()})).then(console.log);
-//# sourceMappingURL=01-fetch-api.b69b7097.js.map

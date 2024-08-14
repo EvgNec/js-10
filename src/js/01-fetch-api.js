67b9218a4a63d62fa61215931077e5ca0fdd41d1
Evgeniy <evgnec@gmail.com>
2024-08-14 23:25:25 +0300

    upd

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -5,3 +5,9 @@ fetch(
     .then(r => r.json())
     .then(console.log);
 /*-------------------------*/
+
+fetch(
+    'http://api.weatherstack.com/current?access_key=d80d26f396a09dee964f29c0205bdd02&query= New York'
+)
+    .then(r => r.json())
+    .then(console.log);

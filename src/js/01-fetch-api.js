3b8e55a27c73e9c4459bd138b57439ba2e67fed9
Evgeniy <evgnec@gmail.com>
2024-08-14 23:30:41 +0300

    upd

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -11,3 +11,37 @@ fetch(
 )
     .then(r => r.json())
     .then(console.log);
+
+ /*-------------------------*/   
+const PASTE_YOUR_API_KEY_HERE = 'd80d26f396a09dee964f29c0205bdd02' 
+   
+const url = 'https://api.weatherstack.com/current?access_key={PASTE_YOUR_API_KEY_HERE}&query=New Delhi';
+const options = {
+	method: 'GET'
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+import axios from 'axios';
+
+const options2 = {
+  method: 'GET',
+  url: 'https://api.weatherstack.com/current?access_key={PASTE_YOUR_API_KEY_HERE}',
+  params: {
+    query: "New Delhi",
+  }
+};
+
+try {
+	const response = await axios.request(options2);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}

1b336e8974364d7515869f7f58301a68c506d0a8
Evgeniy <evgnec@gmail.com>
2024-11-23 18:16:06 +0200

    upd

--- src/js/09-catefind.js
+++ src/js/09-catefind.js
@@ -1,9 +1,13 @@
 const refs = {
   BASE_URL: 'https://api.thecatapi.com/v1',
+  URL : `https://api.thecatapi.com/v1/images/search?limit=20`,
   ENDPOINT: '/breeds',
   KEY: 'live_6mSRucqNyrSNqpDecLOPuWFbMXAyH4nVJnDAVE1MvYAWkJLHdyznA0qqn2NkF7EW',
 // target: document.querySelector('.js-guard'),
+  list: document.querySelector('.js-list'),
+  btnLoadMore: document.querySelector('.js-load-more'),
 };
+
 
 
 // const options = {
@@ -16,5 +20,30 @@ const refs = {
 // fetch('https://api.meest.com/v3.0/openAPI/', options).then(resp => {
 //  console.log("resp.json()", resp.json())
 // });
-
+ fetch(refs.URL,{headers: {
+      'x-api-key': refs.KEY
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+  let imagesData = data;
+  imagesData.map(function(imageData) {
+    
+    let image = document.createElement('img');
+    //use the url from the image object
+    image.src = `${imageData.url}`;
+        
+    let gridCell = document.createElement('div');
+    gridCell.classList.add('col');
+    gridCell.classList.add('col-lg');
+    gridCell.appendChild(image)
+      
+    document.getElementById('grid').appendChild(gridCell);
+    
+    });
+})
+.catch(function(error) {
+   console.log(error);
+});
    

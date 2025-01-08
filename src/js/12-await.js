71526185931a054a06ab69660e67ecce23466014
Evgeniy <evgnec@gmail.com>
2025-01-08 21:19:27 +0200

    upd

--- src/js/12-await.js
+++ src/js/12-await.js
@@ -28,22 +28,32 @@
 //   .then(data => console.log(data))
 //   .catch(e => console.log(e));
 
-
-
-
 async function getVapital() {
-try {
+//   try {
     const URL = 'https://restcountries.com/v3.1/name/';
-    const arr = ['Ukraine', 'France', 'Germany']
+    const arr = ['Ukraine', 'France1', 'Germany'];
+
+    const responses = arr.map(async ctr => {
+        const resp = await fetch(`${URL}${ctr}`);
+       console.log("🚀 ~ responses ~ resp:", resp)     
+      if (!resp.ok) {
     
-    const responses = arr.map(async (ctr) => {
-        const resp = await fetch(`${URL}${ctr}`)
-        return resp.json()
-    })
-    const prom = await Promise.allSettled(responses)
-      console.log("🚀 ~ getVapital ~ prom:", prom)
-      } catch (e) {
-    console.log('🚀 ~ getVapital ~ e:', e);
-  }
+        throw new Error('Not found');
+        // Promise.reject('Not found');
+      }
+      return resp.json();
+    });
+      const prom = await Promise.allSettled(responses)
+      return prom;
+//   } catch (e) {
+//     console.log('🚀 ~ getVapital ~ e:', e);
+//   }
 }
 getVapital()
+    .then(data => {
+        const res = data.filter(({ status }) => status === 'fulfilled').map(({value}) => value[0]);
+        const rej = data.filter(({ status }) => status === 'rejected');
+        console.log(res);
+         console.log(rej);
+    })
+  .catch(e => console.log(e));

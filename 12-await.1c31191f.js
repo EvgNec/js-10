fb77583750cf471abe109d34d6dbba576b3ebe98
EvgNec <evgnec@gmail.com>
2025-01-09 18:51:44 +0000

    Deploying to gh-pages from @ EvgNec/js-10@9788c79763883474766981558526c4115bb10d61 🚀

---
+++ 12-await.1c31191f.js
@@ -0,0 +1,2 @@
+!function(){var e={btnAddCountry:document.querySelector(".js-add"),listCountry:document.querySelector(".js-list"),formCountry:document.querySelector(".js-form")};addCountry.addEventListener("click",(function(){e.formCountry.insertAdjacentHTML("beforeend")}))}();
+//# sourceMappingURL=12-await.1c31191f.js.map
---
+++ 12-await.28290c45.js
@@ -0,0 +1,2 @@
+const e={btnAddCountry:document.querySelector(".js-add"),listCountry:document.querySelector(".js-list"),formCountry:document.querySelector(".js-form")};addCountry.addEventListener("click",(function(){e.formCountry.insertAdjacentHTML("beforeend")}));
+//# sourceMappingURL=12-await.28290c45.js.map
